c84c0b5bd52a2c341ada9e563652e3e2ce33e8e8
dixit <patoliyadixit257@gamil.com>
2023-06-05 20:48:44 +0530

    q

--- lib/screen/view/homeScreen.dart
+++ lib/screen/view/homeScreen.dart
@@ -19,15 +19,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    Provider.of<RandomProvider>(context,listen: false).randomGet();
+    Provider.of<RandomProvider>(context, listen: false).randomGet();
   }
 
   @override
   Widget build(BuildContext context) {
-
     prividerF = Provider.of<RandomProvider>(context, listen: false);
     prividerT = Provider.of<RandomProvider>(context, listen: true);
-
 
     return SafeArea(
       child: Scaffold(
@@ -37,45 +35,94 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Column(
           children: [
-
-            CachedNetworkImage(
-              // placeholder: (context, url) => const CircularProgressIndicator(),
-              imageUrl: '${prividerF!.randomModel!.results![0].picture!.large}',
+            Container(
+              child: Column(
+                children: [
+                  // CachedNetworkImage(
+                  //     imageUrl:
+                  //         '${prividerF!.randomModel!.results![0].picture!.large}',
+                  //   imageBuilder: (context, imageProvider) => Container(
+                  //     height: 200,
+                  //     width: 400,
+                  //     decoration: BoxDecoration(
+                  //       image: DecorationImage(
+                  //         image: imageProvider
+                  //       )
+                  //     ),
+                  //
+                  //   ),
+                  //   placeholder: (context, url) => Container(
+                  //     height: 200,
+                  //     width: 200,
+                  //     color: Colors.red,
+                  //     child: CircularProgressIndicator(),
+                  //   ),
+                  //   errorWidget: (context, url, error) => Text('$error'),
+                  // )
+                ],
+              ),
             ),
+            // CachedNetworkImage(
+            //   // placeholder: (context, url) => const CircularProgressIndicator(),
+            //   imageUrl: '${prividerF!.randomModel!.results![0].picture!.large}',
+            // ),
 
+            ClipRRect(
+              child: CachedNetworkImage(imageUrl: '${prividerF!.randomModel!.results![0].picture!.large}',
+                progressIndicatorBuilder: (context, url, progress) => Center(
+                  child: Image.asset('assets/image/img.png',),
+                )
+
+              ),
+            ),
             Row(
               children: [
-
-                  // child: Image.network('${prividerF!.randomModel!.results![0].picture!.large}',height: 100,width: 100,fit: BoxFit.fill,),
-
+                // child: Image.network('${prividerF!.randomModel!.results![0].picture!.large}',height: 100,width: 100,fit: BoxFit.fill,),
               ],
             ),
             Text("${prividerF!.randomModel!.info!.seed}"),
-            tile(da: 'id',ti:'${prividerF!.randomModel!.results![0].id!.name}' ),
-            tile(da: 'Address',ti:'${prividerF!.randomModel!.results![0].location!.country}-${prividerF!.randomModel!.results![0].location!.city}-${prividerF!.randomModel!.results![0].location!.street!.number}' ),
-            tile(da: 'Age',ti:' ${prividerF!.randomModel!.results![0].dob!.age}'),
-            tile(da: 'Mobile No',ti: '${prividerF!.randomModel!.results![0].phone}'),
-            tile(da: 'Email',ti: '${prividerF!.randomModel!.results![0].email}'),
+            tile(
+                da: 'id',
+                ti: '${prividerF!.randomModel!.results![0].id!.name}'),
+            tile(
+                da: 'Address',
+                ti: '${prividerF!.randomModel!.results![0].location!.country}-${prividerF!.randomModel!.results![0].location!.city}-${prividerF!.randomModel!.results![0].location!.street!.number}'),
+            tile(
+                da: 'Age',
+                ti: ' ${prividerF!.randomModel!.results![0].dob!.age}'),
+            tile(
+                da: 'Mobile No',
+                ti: '${prividerF!.randomModel!.results![0].phone}'),
+            tile(
+                da: 'Email',
+                ti: '${prividerF!.randomModel!.results![0].email}'),
             // tile(),
             // tile(),
           ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          prividerF!.randomGet();
-        },child: Icon(Icons.refresh_rounded),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            prividerF!.randomGet();
+          },
+          child: Icon(Icons.refresh_rounded),
+        ),
       ),
     );
-
   }
-  Widget tile ({String? da, String? ti})
-  {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text("$da"),
-        Text("$ti"),
 
-      ],
+  Widget tile({String? da, String? ti}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "$da",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Text("$ti"),
+        ],
+      ),
     );
   }
 }
